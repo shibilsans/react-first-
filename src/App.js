81fc8e5069b4da4957896cc416a93e8a8f5139c2
shibilsan <muahmmedshibily33@gmail.com>
2024-06-29 21:15:48 +0530

    created new file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{backgroundColor:"red",width:"100px",height:"100px"}}>
+      <h1>hai</h1>
+      
+          </div>
+  );
+}
+
+export default App;
